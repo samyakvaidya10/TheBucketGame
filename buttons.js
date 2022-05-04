63838777871142d6b6c46b39408f00d063a3d5e6
samyakvaidya10 <samyakmcv@gmail.com>
2022-05-05 02:44:23 +0530

    Added restriction for bucket movement

--- buttons.js
+++ buttons.js
@@ -1,15 +1,29 @@
-var pos=0;
+var pos=40;
+var bucketPos=document.getElementById("box");
+var limit=document.getElementById("border");
 function moveRight(){
-    pos=pos+10;
-    var pos_string=pos+"px";
-    document.getElementById("box").style.position="relative";
-    document.getElementById("box").style.left=pos_string;
+    if(((bucketPos.offsetLeft+bucketPos.offsetWidth)+15)>limit.offsetWidth){
+
     }
+    else{
+        pos=pos+10;
+        var pos_string=pos+"%";
+        document.getElementById("box").style.position="relative";
+        document.getElementById("box").style.left=pos_string;
+    
+    }
+}
     
 function moveLeft(){
-    pos=pos-10;
-    var pos_string=pos+"px";
-    document.getElementById("box").style.position="relative";
-    document.getElementById("box").style.left=pos_string;
+    if(((bucketPos.offsetLeft)-15)<0){
+
     }
+    else{
+        pos=pos-10;
+        var pos_string=pos+"%";
+        document.getElementById("box").style.position="relative";
+        document.getElementById("box").style.left=pos_string;
+    }
+    
+}
 
