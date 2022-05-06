f132350dee4f7c7b12ec092f1f9b5e42a4cbac4a
samyakvaidya10 <samyakmcv@gmail.com>
2022-05-06 16:12:25 +0530

    optimised moveLeft() function

--- buttons.js
+++ buttons.js
@@ -2,23 +2,16 @@ var pos=40;
 var bucketPos=document.getElementById("box");
 var limit=document.getElementById("border");
 function moveRight(){
-    if(((bucketPos.offsetLeft+bucketPos.offsetWidth)+15)>limit.offsetWidth){
-
-    }
-    else{
+    if(!(((bucketPos.offsetLeft+bucketPos.offsetWidth)+25)>limit.offsetWidth)){
         pos=pos+10;
         var pos_string=pos+"%";
         document.getElementById("box").style.position="relative";
         document.getElementById("box").style.left=pos_string;
-    
     }
 }
     
 function moveLeft(){
-    if(((bucketPos.offsetLeft)-15)<0){
-
-    }
-    else{
+    if(!(((bucketPos.offsetLeft)-15)<0)){
         pos=pos-10;
         var pos_string=pos+"%";
         document.getElementById("box").style.position="relative";
