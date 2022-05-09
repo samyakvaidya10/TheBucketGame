c3c75306a43bc2ab49668b9d5a2f868a8a5e8623
samyakvaidya10 <samyakmcv@gmail.com>
2022-05-09 21:59:51 +0530

    Added randomly generated drops

--- buttons.js
+++ buttons.js
@@ -1,9 +1,11 @@
 var pos=0;
 var bucketPos=document.getElementById("box");
 var limitBor=document.getElementById("limit");
+var dropPos=document.getElementById("drop");
 var step;
 var limit;
 calibration();
+dropping(limitBor.offsetLeft,limitBor.offsetWidth);
 function moveRight(){
     if(!(((bucketPos.offsetLeft+bucketPos.offsetWidth)+step)>limit)){
         pos=pos+10;
@@ -29,4 +31,15 @@ function calibration(){
     //console.log(step);
     limit=((step*8)+bucketPos.offsetWidth);
     limitBor.style.width=((limit-10)+"px");
+}
+
+function dropping(start, end){
+    console.log(limitBor.offsetLeft);
+    console.log(limitBor.offsetWidth);
+    var newDropPos=Math.random(end-start)*100;
+    console.log(newDropPos);
+    dropPos.style.position="relative";
+    var newpos_string=newDropPos+"%";
+    dropPos.style.left=newpos_string;
+    console.log(dropPos.offsetLeft)
 }
